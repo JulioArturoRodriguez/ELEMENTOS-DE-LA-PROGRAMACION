b530332e8a6e617a21d6ce1082757c5e2ecfe935
JulioArturoRodriguez <140935997+JulioArturoRodriguez@users.noreply.github.com>
2024-07-01 22:36:47 -0300

    Add files via upload

---
+++ programa de partica/calculadora de sueldo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int n1, n2, sueldo;
+main
+{
+    printf("ingrese la cantidad de horas trabajadas por favor:/n%d",'n1');
+    scanf("%d",'&n1');
+    printf("ingrese el valor x hora:/n %d",'n2');
+    scanf("%d",'&n2');
+    sueldo==(n1+n2);
+    printf("su sueldo es:%d",'suldo');
+    return (0);
+}
